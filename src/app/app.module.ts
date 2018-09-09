d11efe1043c4f33f6ea36866c06801fc5a83f5e1
shorbagymo4 <mohamed.elshorbagy@mo4group.com>
2018-09-09 10:20:42 +0200

    Add Pro Client Plugin inside app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,34 @@ import { HomePage } from '../pages/home/home';
 import { QRScanner } from '@ionic-native/qr-scanner'
 import { UserDataPage } from '../pages/user-data/user-data';
 import { UserDataProvider } from '../providers/user-data/user-data';
+
+import { Pro } from '@ionic/pro';
+import { Injectable, Injector } from '@angular/core';
+
+Pro.init('7c3b77a3', {
+  appVersion: '1.1'
+})
+
+@Injectable()
+export class MyErrorHandler implements ErrorHandler {
+  ionicErrorHandler: IonicErrorHandler;
+
+  constructor(injector: Injector) {
+    try {
+      this.ionicErrorHandler = injector.get(IonicErrorHandler);
+    } catch (e) {
+      // Unable to get the IonicErrorHandler provider, ensure
+      // IonicErrorHandler has been added to the providers list below
+    }
+  }
+
+  handleError(err: any): void {
+    Pro.monitoring.handleNewError(err);
+    // Remove this if you want to disable Ionic's auto exception handling
+    // in development mode.
+    this.ionicErrorHandler && this.ionicErrorHandler.handleError(err);
+  }
+}
 @NgModule({
   declarations: [
     MyApp,
@@ -32,6 +60,8 @@ import { UserDataProvider } from '../providers/user-data/user-data';
     StatusBar,
     SplashScreen,
     { provide: ErrorHandler, useClass: IonicErrorHandler },
+    IonicErrorHandler,
+    [{ provide: ErrorHandler, useClass: MyErrorHandler }],
     QRScanner,
     UserDataProvider
   ]
